aefe435d2db9b5ee1671b774da7187b652fba30e
naseriax <naramnejad@gmail.com>
2025-04-04 12:55:34 +0200

    init

---
+++ main.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type Config struct {
+	IP       string
+	Port     string
+	Username string
+	Password string
+	Path     string
+	File     string
+	Output   string
+	Mode     string
+	Key      string
+	Timeout  int
+}
+
+func main() {
+	config := Config{}
+	flag.StringVar(&config.IP, "ip", "", "IP address of the NETCONF device (required)")
+	flag.StringVar(&config.Port, "port", "830", "Port number for NETCONF connection")
+	flag.StringVar(&config.Username, "username", "admin", "Username for authentication")
+	flag.StringVar(&config.Password, "password", "", "Password for authentication (required)")
+	flag.StringVar(&config.File, "file", "", "Path to XML file containing NETCONF RPC payload")
+	flag.StringVar(&config.Mode, "mode", "exec", "Execution modes: get_capabilities, exec ,get_session_id")
+	flag.StringVar(&config.Output, "output", "", "Path to output file for NETCONF response (optional)")
+	flag.StringVar(&config.Key, "key", "", "ssh key file(optional)")
+	flag.IntVar(&config.Timeout, "timeout", 30, "Connection timeout in seconds")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "\nA NETCONF client to interact with network devices\n")
+		fmt.Fprintf(os.Stderr, "\nOptions:\n")
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\nExamples:\n")
+		fmt.Fprintf(os.Stderr, "  # Using inline path, output to console\n")
+		fmt.Fprintf(os.Stderr, "  %s -ip 192.168.1.1 -username admin -password secret -path '<get-config><source><running/></source></get-config>'\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  # Using XML file, output to file\n")
+		fmt.Fprintf(os.Stderr, "  %s -ip 192.168.1.1 -username admin -password secret -file rpc.xml -output response.xml\n", os.Args[0])
+	}
+
+	flag.Parse()
+
+	if err := validateConfig(config); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := runNetconfClient(config); err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+}
+
+func validateConfig(config Config) error {
+	if config.IP == "" || config.Password == "" {
+		return fmt.Errorf("IP address and password are required")
+	}
+	if config.Path != "" && config.File != "" {
+		return fmt.Errorf("cannot specify both -path and -file; choose one")
+	}
+	if config.Path == "" && config.File == "" {
+		return fmt.Errorf("either -path or -file must be specified")
+	}
+	return nil
+}
+
+func runNetconfClient(config Config) error {
+
+	ncEndPoint := Endpoint{
+		Ip:          config.IP,
+		Username:    config.Username,
+		Password:    config.Password,
+		PrivKeyPath: config.Key,
+		Timeout:     10,
+		Port:        config.Port,
+	}
+
+	if err := ncEndPoint.Connect(); err != nil {
+		log.Fatalln(err)
+	}
+	defer ncEndPoint.Disconnect()
+
+	err := os.WriteFile(config.IP+"_capabilities.xml", []byte(formatXML(ncEndPoint.Capabilities, true)), 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write response to file %s: %v", config.Output, err)
+	}
+
+	formattedResponse := ""
+
+	rpc, err := getRPCPayload(config)
+	if err != nil {
+		return fmt.Errorf("failed to get RPC payload: %v", err)
+	}
+
+	reply, err := ncEndPoint.Run(rpc)
+	if err != nil {
+		return fmt.Errorf("failed to execute NETCONF RPC: %v", err)
+	}
+
+	formattedResponse = formatXML(reply, true)
+
+	if config.Output != "" {
+		err = os.WriteFile(config.Output, []byte(formattedResponse), 0644)
+		if err != nil {
+			return fmt.Errorf("failed to write response to file %s: %v", config.Output, err)
+		}
+		fmt.Printf("Response written to %s\n", config.Output)
+	} else {
+		fmt.Println("NETCONF Response:")
+		fmt.Println(formattedResponse)
+	}
+
+	return nil
+}
+
+func removeEmptyLines(s string) string {
+	lines := strings.Split(s, "\n")
+	var b strings.Builder
+	b.Grow(len(s))
+
+	first := true
+	for _, line := range lines {
+		if line != "" {
+			if !first {
+				b.WriteString("\n")
+			}
+			b.WriteString(line)
+			first = false
+		}
+	}
+	return b.String()
+}
+
+func getRPCPayload(config Config) (string, error) {
+	if config.File != "" {
+		data, err := os.ReadFile(config.File)
+		if err != nil {
+			return "", fmt.Errorf("failed to read XML file %s: %v", config.File, err)
+		}
+		return removeEmptyLines(string(data)), nil
+	}
+	return config.Path, nil
+}
+
+func formatXML(data string, removeBlankLines bool) string {
+	var b strings.Builder
+	if removeBlankLines {
+		b.Grow(len(data))
+	} else {
+		b.Grow(len(data) + len(data)/2)
+	}
+
+	indent := 0
+	for i := range len(data) {
+		switch data[i] {
+		case '<':
+			if i+1 < len(data) && data[i+1] == '/' {
+				indent--
+				if !removeBlankLines {
+					b.WriteString("\n")
+					b.WriteString(indentString(indent))
+				}
+				b.WriteByte('<')
+			} else {
+				if !removeBlankLines {
+					b.WriteString("\n")
+					b.WriteString(indentString(indent))
+				}
+				b.WriteByte('<')
+				indent++
+			}
+		case '>':
+			b.WriteByte('>')
+			if !removeBlankLines && i+1 < len(data) && data[i+1] == '<' {
+				b.WriteString("\n")
+				b.WriteString(indentString(indent))
+			}
+		default:
+			b.WriteByte(data[i])
+		}
+	}
+	return b.String()
+}
+
+func indentString(level int) string {
+	const indentUnit = "  "
+	return strings.Repeat(indentUnit, level)
+}
---
+++ pssh.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/crypto/ssh"
+)
+
+type Endpoint struct {
+	Ip           string
+	Name         string
+	Username     string
+	Password     string
+	PrivKeyPath  string
+	Port         string
+	SshOut       io.Reader
+	SshIn        io.WriteCloser
+	Timeout      int
+	Client       *ssh.Client
+	Session      *ssh.Session
+	Capabilities string
+}
+
+func publicKeyFile(file string) ssh.AuthMethod {
+	buffer, err := os.ReadFile(file)
+	if err != nil {
+		return nil
+	}
+
+	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		return nil
+	}
+	return ssh.PublicKeys(key)
+}
+
+// Connect connects to the specified server and opens a session (Filling the Client and Session fields in SshAgent struct).
+func (s *Endpoint) Connect() error {
+	if err := validateNode(s); err != nil {
+		return err
+	}
+
+	var err error
+	config := &ssh.ClientConfig{
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         time.Duration(s.Timeout) * time.Second,
+	}
+
+	config.User = s.Username
+
+	authMethods := []ssh.AuthMethod{
+		ssh.Password(s.Password),
+	}
+
+	if s.PrivKeyPath != "" {
+		authMethods = append(authMethods, publicKeyFile(s.PrivKeyPath))
+	}
+
+	config.Auth = authMethods
+
+	s.Client, err = ssh.Dial("tcp", fmt.Sprintf("%v:%v", s.Ip, s.Port), config)
+	if err != nil {
+		return fmt.Errorf("%v:%v - %v", s.Ip, s.Port, err.Error())
+	}
+
+	if err := s.cliLogin(); err != nil {
+		return err
+	}
+
+	helloPayload := `
+	<hello xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+  		<capabilities>
+    		<capability>urn:ietf:params:netconf:base:1.0</capability>
+  		</capabilities>
+	</hello>
+	]]>]]>`
+
+	s.Run(helloPayload)
+
+	return nil
+}
+
+// CliLogin does the special login sequence needed to login to PSS cli.
+func (s *Endpoint) cliLogin() error {
+	var err error
+
+	s.Session, err = s.Client.NewSession()
+	if err != nil {
+		return fmt.Errorf("%v:%v - failure on Client.NewSession() - details: %v", s.Ip, s.Port, err.Error())
+	}
+
+	err = s.Session.RequestSubsystem("netconf")
+	if err != nil {
+		log.Fatalf("Failed to request netconf subsystem: %v", err)
+	}
+
+	s.SshIn, err = s.Session.StdinPipe()
+	if err != nil {
+		log.Fatalf("Failed to get stdin: %v", err)
+	}
+	s.SshOut, err = s.Session.StdoutPipe()
+	if err != nil {
+		log.Fatalf("Failed to get stdout: %v", err)
+	}
+
+	helloMsg := `<?xml version="1.0" encoding="UTF-8"?>
+	<hello xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+	  <capabilities>
+		<capability>urn:ietf:params:netconf:base:1.0</capability>
+	  </capabilities>
+	</hello>]]>]]>`
+
+	_, err = s.SshIn.Write([]byte(helloMsg))
+	if err != nil {
+		log.Fatalf("Failed to send hello message: %v", err)
+	}
+
+	var responseBuf bytes.Buffer
+	buf := make([]byte, 1024)
+	delimiter := []byte("]]>]]>")
+	for {
+		n, err := s.SshOut.Read(buf)
+		if err != nil && err != io.EOF {
+			log.Fatalf("Failed to read response: %v", err)
+		}
+		if n > 0 {
+			responseBuf.Write(buf[:n])
+			if bytes.Contains(responseBuf.Bytes(), delimiter) {
+				break
+			}
+		}
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	s.Capabilities = responseBuf.String()
+
+	return nil
+
+}
+
+// Run executes the given cli command on the opened session.
+func (s *Endpoint) Run(arg string) (string, error) {
+
+	if !strings.Contains(arg, "]]>]]>") {
+		arg = arg + "]]>]]>"
+	}
+
+	_, err := s.SshIn.Write([]byte(arg))
+	if err != nil {
+		log.Fatalf("Failed to send the rpc message: %v", err)
+	}
+
+	var responseBuf bytes.Buffer
+	buf := make([]byte, 1024)
+	delimiter := []byte("]]>]]>")
+	for {
+		n, err := s.SshOut.Read(buf)
+		if err != nil && err != io.EOF {
+			log.Fatalf("Failed to read response: %v", err)
+		}
+		if n > 0 {
+			responseBuf.Write(buf[:n])
+			if bytes.Contains(responseBuf.Bytes(), delimiter) {
+				break
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return responseBuf.String(), nil
+}
+
+// Disconnect closes the ssh sessoin and connection.
+func (s *Endpoint) Disconnect() {
+
+	closePayload := `
+	<rpc message-id="103" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+  		<close-session/>
+	</rpc>
+	]]>]]>
+	`
+	s.Run(closePayload)
+	s.Session.Close()
+	s.Client.Close()
+}
+
+func validateIpAddress(ip string) error {
+	ipSegments := strings.Split(ip, ".")
+	if len(ipSegments) != 4 {
+		return fmt.Errorf("provided ip: %v - ip address is not formatted properly", ip)
+	}
+	for _, seg := range ipSegments {
+		num, err := strconv.Atoi(seg)
+		if err != nil {
+			return fmt.Errorf("provided ip: %v - ip address includes wrong values: %v", ip, seg)
+		} else {
+			if num < 0 || num > 255 {
+				return fmt.Errorf("provided ip: %v - ip address includes wrong values: %v", ip, seg)
+			}
+		}
+	}
+
+	return nil
+}
+
+func validateNode(s *Endpoint) error {
+	s.Timeout = 30
+	if err := validateIpAddress(s.Ip); err != nil {
+		return err
+	}
+	if _, err := strconv.Atoi(s.Port); err != nil {
+		log.Printf("provided port: %v - wrong port number, defaulting to 22", s.Port)
+		s.Port = "22"
+	}
+
+	return nil
+}
